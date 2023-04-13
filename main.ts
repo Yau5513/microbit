0cd993b2944691093aa52c2935e8b69d63d46fc4
Yau5513 <116283375+Yau5513@users.noreply.github.com>
2023-04-13 19:41:15 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    answer = "" + answer + "A"
+})
+input.onButtonPressed(Button.AB, function () {
+    answer = ""
+})
+input.onButtonPressed(Button.B, function () {
+    answer = "" + answer + "B"
+})
+let answer = ""
+let password = "AABAA"
+answer = ""
